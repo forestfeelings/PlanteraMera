5d7cd2555be20a1f6cd10d729569de34aea8eeb8
Erica <erica.i.gustafsson@gmail.com>
2020-05-04 12:55:24 +0200

    Basic controller, views, view model, bootstrap

---
+++ Controllers/AppController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlanteraMera.Services;
+using PlanteraMera.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanteraMera.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService _mailService;
+
+        public AppController(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("kontakt")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost("kontakt")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Send email
+                _mailService.SendMessage("erica.i.gustafsson@gmail.com", model.Subject, $"From: {model.Name} - {model.Email}, Message: {model.Message}");
+                ViewBag.UserMessage = "Meddelandet skickades!";
+                ModelState.Clear();
+            }
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Title = "Om oss";
+
+            return View();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PlanteraMera.Services;
 
 namespace PlanteraMera
 {
@@ -16,24 +17,34 @@ namespace PlanteraMera
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IMailService, NullMailService>();
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/error");
+            }
+
+            app.UseStaticFiles();
+            app.UseNodeModules();
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            app.UseEndpoints(cfg =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                cfg.MapControllerRoute("Fallback",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "App", action = "Index" });
             });
         }
     }
